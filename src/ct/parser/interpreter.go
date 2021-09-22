eb7c56f6687d955cc1dedfdece10e0a8e731bd18
Xin Shen <xin.shen@sanofi.com>
2021-09-22 16:02:04 +0800

    fix UnicodeIndex not working for repeated words

--- src/ct/parser/interpreter.go
+++ src/ct/parser/interpreter.go
@@ -38,52 +38,54 @@ func (i *Interpreter) Interpret(input string) (relation.Relations, relation.Rela
 	list.FixMissingVariable()
 	trees := i.buildTrees(list)
 	orRs, andRs := trees.Relations()
+	m := make(map[string]int)
 	for _, listVal := range list {
 		for _, item := range listVal {
 			for _, orR := range orRs {
 				if item.val == orR.Name {
-					orR.Start = UnicodeIndex(input, item.val)
+					orR.Start = UnicodeIndex(input, item.val, m)
 					orR.End = orR.Start + len(item.val)
 				}
 				if orR.Lower != nil && orR.Lower.Value == item.val {
 					if orR.Lower.Start == 0 {
-						orR.Lower.Start = UnicodeIndex(input, item.val)
+						orR.Lower.Start = UnicodeIndex(input, item.val, m)
 						orR.Lower.End = orR.Lower.Start + len(item.val)
 					}
 				}
 				if orR.Upper != nil && orR.Upper.Value == item.val {
 					if orR.Upper.Start == 0 {
-						orR.Upper.Start = UnicodeIndex(input, item.val)
+						orR.Upper.Start = UnicodeIndex(input, item.val, m)
 						orR.Upper.End = orR.Upper.Start + len(item.val)
 					}
 				}
 				if orR.Unit != nil && orR.Unit.Value == item.val && item.val != "" {
-					orR.Unit.Start = append(orR.Unit.Start, UnicodeIndex(input, item.val))
-					orR.Unit.End = append(orR.Unit.End, UnicodeIndex(input, item.val)+len(item.val))
+					startIndex := UnicodeIndex(input, item.val, m)
+					orR.Unit.Start = append(orR.Unit.Start, startIndex)
+					orR.Unit.End = append(orR.Unit.End, startIndex + len(item.val))
 				}
 			}
 			for _, andR := range andRs {
 				if item.val == andR.Name {
-					andR.Start = UnicodeIndex(input, item.val)
+					andR.Start = UnicodeIndex(input, item.val, m)
 					andR.End = andR.Start + len(item.val)
 				}
 				if andR.Lower != nil && andR.Lower.Value == item.val {
 					if andR.Lower.Start == 0 {
-						andR.Lower.Start = UnicodeIndex(input, item.val)
+						andR.Lower.Start = UnicodeIndex(input, item.val, m)
 						andR.Lower.End = andR.Lower.Start + len(item.val)
 					}
 				}
 				if andR.Upper != nil && andR.Upper.Value == item.val {
 					if andR.Upper.Start == 0 {
-						andR.Upper.Start = UnicodeIndex(input, item.val)
+						andR.Upper.Start = UnicodeIndex(input, item.val, m)
 						andR.Upper.End = andR.Upper.Start + len(item.val)
 					}
 				}
 
 				if andR.Unit != nil && andR.Unit.Value == item.val && item.val != "" {
-					// log.Printf("%+v-%+v-%+v-%+v", andR.Unit.Value, item.val, item.pos, UnicodeIndex(input, item.val))
-					andR.Unit.Start = append(andR.Unit.Start, UnicodeIndex(input, item.val))
-					andR.Unit.End = append(andR.Unit.End, UnicodeIndex(input, item.val)+len(item.val))
+					startIndex := UnicodeIndex(input, item.val, m)
+					andR.Unit.Start = append(andR.Unit.Start, startIndex)
+					andR.Unit.End = append(andR.Unit.End, startIndex + len(item.val))
 				}
 			}
 		}
@@ -103,12 +105,18 @@ func (i *Interpreter) buildTrees(list List) Trees {
 }
 
 //get unicode index of substring
-func UnicodeIndex(str, substr string) int {
+func UnicodeIndex(str, substr string, m map[string]int) int {
+	lastIndex := m[substr]
+	if lastIndex != 0 {
+		lastIndex += len(substr)
+	}
+	str = str[lastIndex:]
 	result := strings.Index(str, substr)
 	if result >= 0 {
 		prefix := []byte(str)[0:result]
 		rs := []rune(string(prefix))
-		result = len(rs)
+		result = len(rs) + lastIndex
+		m[substr] = result 
 	}
 	return result
 }
